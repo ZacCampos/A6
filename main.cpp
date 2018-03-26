48af8219be486d72576cd29338e2e8a5f5783a48
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-03-25 22:29:25 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,76 @@
+/*******************************************************************************
+ * AUTHOR		: Zac Campos
+ * STUDENT ID	: 1058476
+ * ASSIGNMENT #6: Nested If-Then-Else Statements
+ * CLASS		: CS1A
+ * SECTION		: MW 8am
+ * DUE DATE		: 3/26/18
+ ******************************************************************************/
+
 #include <iostream>
 #include <iomanip>
+#include <cstring>
 using namespace std;
 
-int main ()
+/*******************************************************************************
+ * NESTED IF-THEN-ELSE STATEMENTS
+ * -----------------------------------------------------------------------------
+ * This program will output the largest of 3 values.
+ * -----------------------------------------------------------------------------
+ * INPUT:
+ * 		<Three values taking in from the user.>
+ *
+ * OUTPUT:
+ * 	<This program will output the largest of the thre values.>
+ ******************************************************************************/
+
+
+int main()
 {
+	/***************************************************************************
+	 * CONSTANTS
+	 * -------------------------------------------------------------------------
+	 * OUTPUT - USED FOR CLASS HEADING
+	 * -------------------------------------------------------------------------
+	 * PROGRAMMER	: Programmer's Name
+	 * CLASS		: Student's Course
+	 * SECTION		: Class Days and Times
+	 * LAB_NUM		: Lab Number (specific to this lab)
+	 * LAB_NAME		: Title of the Lab
+	 **************************************************************************/
+
+	const char PROGRAMMER[] = "Zac Campos";
+	const char CLASS[]		= "CS1A";
+	const char SECTION[]	= "MW: 8am";
+	const int  ASSIGN_NUM	= 6;
+	const char ASSIGN_NAME[]= "Nested If-Then-Else Statements";
+
+	const int C_STRING_SIZE = 10;
+	const char FIRST_VAL[]	= "1st value";
+	const char SECOND_VAL[]	= "2nd value";
+	const char THIRD_VAL[]	= "3rd value";
+
+	//INPUT & CALCULATION VARIABLES
 	float firstFloat 	= 0;
 	float secondFloat 	= 0;
 	float thirdFloat 	= 0;
+	char highestValueString[C_STRING_SIZE];
+	float highestValue;
 
-	//Input
+	// OUTPUT - Class Heading
+	cout << left;
+	cout << "***************************************************\n";
+	cout << "*  PROGRAMMED BY : "	<< PROGRAMMER 	<< endl;
+	cout << "*  "		<< setw(14) << "CLASS"		<< ": " << CLASS	<< endl;
+	cout << "*  "		<< setw(14)	<< "SECTION"	<< ": " << SECTION  << endl;
+	cout << "*  ASSIGNMENT #" << setw(9) << ASSIGN_NUM	<< ": "	<< ASSIGN_NAME;
+	cout << endl;
+	cout << "***************************************************\n\n";
+	cout << right;
+
+	/***************************************************************************
+	 * INPUT - describe input here
+	 **************************************************************************/
 	cout << "Value 1: ";
 	cin  >> firstFloat;
 	cin.ignore(10000, '\n');
@@ -21,36 +83,45 @@ int main ()
 	cin  >> thirdFloat;
 	cin.ignore(10000, '\n');
 	cout << endl;
-
-	//Processing and Output
+	/***************************************************************************
+	 * PROCESSING - describe processing here
+	 **************************************************************************/
 	if (firstFloat > secondFloat)
 	{
 		if (firstFloat > thirdFloat)
 		{
-			cout << fixed << setprecision(2);
-			cout << "The largest value is the 1st value: " << firstFloat;
+			highestValue = firstFloat;
+			strncpy(highestValueString, FIRST_VAL, C_STRING_SIZE);
 		}
 		else
 		{
-			cout << fixed << setprecision(2);
-			cout << "The largest value is the 3rd value: " << thirdFloat;
+			highestValue = thirdFloat;
+			strncpy(highestValueString, THIRD_VAL, C_STRING_SIZE);
 		}
 	}
 	else
 	{
 		if (secondFloat > thirdFloat)
 		{
-			cout << fixed << setprecision(2);
-			cout << "The largest value is the 2nd value: " << secondFloat;
+			highestValue = secondFloat;
+			strncpy(highestValueString, SECOND_VAL, C_STRING_SIZE);
 		}
 		else
 		{
-			cout << fixed << setprecision(2);
-			cout << "The largest value is the 3rd value: " << thirdFloat;
+			highestValue = thirdFloat;
+			strncpy(highestValueString, THIRD_VAL, C_STRING_SIZE);
 		}
 	}
 
+	// OUTPUT
+	cout << fixed << setprecision(2);
+	cout << "The largest value is the " << highestValueString << ": "
+		 << highestValue;
 
 	return 0;
 }
+
+
+
+
 
